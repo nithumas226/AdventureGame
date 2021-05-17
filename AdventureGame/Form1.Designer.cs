7d3a7e5edf301eca96acfbf93fa4afd16c875db0
nithumas226 <nithumas226@outlook.com>
2021-05-17 11:30:57 -0400

    Add project files.

---
+++ AdventureGame/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace AdventureGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.imageLabel = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.option3Button = new System.Windows.Forms.Button();
+            this.button1Label = new System.Windows.Forms.Label();
+            this.button2Label = new System.Windows.Forms.Label();
+            this.button3Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Garamond", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(40, 36);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(597, 90);
+            this.outputLabel.TabIndex = 0;
+            // 
+            // imageLabel
+            // 
+            this.imageLabel.Location = new System.Drawing.Point(37, 126);
+            this.imageLabel.Name = "imageLabel";
+            this.imageLabel.Size = new System.Drawing.Size(600, 155);
+            this.imageLabel.TabIndex = 1;
+            this.imageLabel.Text = "label1";
+            // 
+            // option1Button
+            // 
+            this.option1Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.option1Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.option1Button.Location = new System.Drawing.Point(40, 316);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(75, 23);
+            this.option1Button.TabIndex = 2;
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // option2Button
+            // 
+            this.option2Button.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("option2Button.BackgroundImage")));
+            this.option2Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.option2Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.option2Button.Location = new System.Drawing.Point(40, 369);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(75, 23);
+            this.option2Button.TabIndex = 3;
+            this.option2Button.UseVisualStyleBackColor = true;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // option3Button
+            // 
+            this.option3Button.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("option3Button.BackgroundImage")));
+            this.option3Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.option3Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.option3Button.Location = new System.Drawing.Point(40, 418);
+            this.option3Button.Name = "option3Button";
+            this.option3Button.Size = new System.Drawing.Size(75, 23);
+            this.option3Button.TabIndex = 4;
+            this.option3Button.UseVisualStyleBackColor = true;
+            this.option3Button.Visible = false;
+            this.option3Button.Click += new System.EventHandler(this.option3Button_Click);
+            // 
+            // button1Label
+            // 
+            this.button1Label.Location = new System.Drawing.Point(134, 316);
+            this.button1Label.Name = "button1Label";
+            this.button1Label.Size = new System.Drawing.Size(503, 23);
+            this.button1Label.TabIndex = 5;
+            this.button1Label.Text = "label1";
+            // 
+            // button2Label
+            // 
+            this.button2Label.Location = new System.Drawing.Point(134, 369);
+            this.button2Label.Name = "button2Label";
+            this.button2Label.Size = new System.Drawing.Size(503, 23);
+            this.button2Label.TabIndex = 6;
+            this.button2Label.Text = "label1";
+            // 
+            // button3Label
+            // 
+            this.button3Label.Location = new System.Drawing.Point(134, 418);
+            this.button3Label.Name = "button3Label";
+            this.button3Label.Size = new System.Drawing.Size(503, 23);
+            this.button3Label.TabIndex = 7;
+            this.button3Label.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(666, 459);
+            this.Controls.Add(this.button3Label);
+            this.Controls.Add(this.button2Label);
+            this.Controls.Add(this.button1Label);
+            this.Controls.Add(this.option3Button);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.imageLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Choose Your Own Adventure Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label imageLabel;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Button option3Button;
+        private System.Windows.Forms.Label button1Label;
+        private System.Windows.Forms.Label button2Label;
+        private System.Windows.Forms.Label button3Label;
+    }
+}
+
---
+++ AdventureGame/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace AdventureGame
+{
+    public partial class Form1 : Form
+    {
+        //tracks page number
+        int page = 1;
+
+        //creates random generator
+        Random RandGen = new Random();
+
+        //creates sound player
+        SoundPlayer musicPlayer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //creates and generates random number
+            int pageNumber;
+            pageNumber = RandGen.Next(1, 5);
+
+            //display opening game text and options
+            DisplayPage();
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            //checks what page you're on and flips to the correct page if option 1 is chosen
+            if (page == 1) { page = 2; }
+            else if (page == 2) { page = 4; }
+            else if (page == 3) { page = 7; }
+            else if (page == 4) { page = 9; }
+            else if (page == 6) { page = 11; }
+            else if (page == 7)
+            {
+                int pageNumber;
+                pageNumber = RandGen.Next(1, 5);
+                if(pageNumber == 1)
+                {
+                    page = 13;
+                }
+                else
+                {
+                    page = 14;
+                }
+            }
+            else if (page == 9) { page = 16; }
+            else if (page == 10) { page = 18; }
+            else if (page == 15) { page = 20; }
+            else if (page == 17) { page = 22; }
+            else if (page == 20) { page = 24; }
+            else if (page == 21) { page = 26; }
+            else if (page == 5) { page = 1; }
+            else if (page == 8) { page = 1; }
+            else if (page == 11) { page = 1; }
+            else if (page == 12) { page = 1; }
+            else if (page == 13) { page = 1; }
+            else if (page == 14) { page = 1; }
+            else if (page == 16) { page = 1; }
+            else if (page == 18) { page = 1; }
+            else if (page == 19) { page = 1; }
+            else if (page == 22) { page = 1; }
+            else if (page == 23) { page = 1; }
+            else if (page == 24) { page = 1; }
+            else if (page == 25) { page = 1; }
+            else if (page == 26) { page = 1; }
+            else if (page == 27) { page = 1; }
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            //checks what page you're on and flips to the correct page if option 2 is chosen
+            if (page == 1) { page = 3; }
+            else if (page == 2) { page = 6; }
+            else if (page == 3) { page = 8; }
+            else if (page == 4) { page = 10; }
+            else if (page == 6) { page = 12; }
+            else if (page == 7) { page = 15; }
+            else if (page == 9) { page = 17; }
+            else if (page == 10) { page = 19; }
+            else if (page == 15) { page = 21; }
+            else if (page == 17) { page = 23; }
+            else if (page == 20) { page = 25; }
+            else if (page == 21) { page = 27; }
+            else if (page == 5) { page = 99; }
+            else if (page == 8) { page = 99; }
+            else if (page == 11) { page = 99; }
+            else if (page == 12) { page = 99; }
+            else if (page == 13) { page = 99; }
+            else if (page == 14) { page = 99; }
+            else if (page == 16) { page = 99; }
+            else if (page == 18) { page = 99; }
+            else if (page == 19) { page = 99; }
+            else if (page == 22) { page = 99; }
+            else if (page == 23) { page = 99; }
+            else if (page == 24) { page = 99; }
+            else if (page == 25) { page = 99; }
+            else if (page == 26) { page = 99; }
+            else if (page == 27) { page = 99; }
+        }
+
+        private void option3Button_Click(object sender, EventArgs e)
+        {
+            //checks what page you're on and flips to the correct page if option 3 is chosen
+            if (page == 2) { page = 5; }
+        }
+
+        public void DisplayPage()
+        {
+            
+        }
+    }
+}
